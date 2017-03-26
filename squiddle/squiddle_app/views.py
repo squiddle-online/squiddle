9c2f5a5b259f1eb454180b1ee3ba84af5568a6ca
Rational Coder <rationalcoder@gmail.com>
2017-03-25 23:30:22 -0500

    cleaned up the html and fixed the dropdown menus.

--- squiddle/squiddle_app/views.py
+++ squiddle/squiddle_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def full_page(request):
-    return render(request, 'squiddle_app/full_page.html', {})
+    return render(request, 'squiddle_app/full_page.html')
 
 
