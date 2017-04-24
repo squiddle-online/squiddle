505348eac71c52fea56e21077d31b28e41fc0a39
Rational Coder <rationalcoder@gmail.com>
2017-04-24 09:46:01 -0500

    Time Block inputs and validation setup.

--- squiddle/squiddle_app/static/edit_free_time.js
+++ squiddle/squiddle_app/static/edit_free_time.js
@@ -1,10 +1,33 @@
+var startTimeErrorList = null;
+var endTimeErrorList = null;
 
 window.addEventListener("load", function() {
-    console.log("init free time");
     document.getElementById("add-time-block-button").addEventListener("click", addTimeBlock);
+
+    startTimeErrorList = document.getElementById("start-time-error-list");
+    endTimeErrorList = document.getElementById("end-time-error-list");
 });
 
 function addTimeBlock() {
     this.blur();
-    console.log("Adding Time Block");
+
+    var rawStartTime = document.getElementById("start-time").value;
+    var rawEndTime = document.getElementById("end-time").value;
+    var timeRegex = /(\d):(\d)/;
+
+    var startMatch = rawStartTime.match(timeRegex);
+    var endMatch = rawEndTime.match(timeRegex);
+
+    console.log("Got here.");
+
+    if (!startMatch)
+        startTimeErrorList.innerHTML = "<li class=\"error\">Invalid Start Time</li>";
+    else
+        startTimeErrorList.innerHtml = "";
+
+    if (!endMatch)
+        endTimeErrorList.innerHTML = "<li class=\"error\">Invalid End Time</li>";
+    else
+        endTimeErrorList.innerHTML = "";
+
 }
