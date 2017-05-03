959a369beadbd92e4c18ad78c6ff1da32e5f5758
Rational Coder <rationalcoder@gmail.com>
2017-05-03 09:19:43 -0500

    Progress on fixing merge error.

--- squiddle/squiddle_app/static/schedule_manager.js
+++ squiddle/squiddle_app/static/schedule_manager.js
@@ -15,15 +15,9 @@ const GroupColorPallet = [
 ];
 
 var ScheduleManager = function(type) {
-    // TODO: grab the schedule table by id and setup anything else.
     var scheduleTable = document.getElementById("table");
     var hourContainer = scheduleTable.getElementsByClassName("hour-container");
-
-
     this.type = type;
-
-    // TODO: the table needs to be invisible by default, so the user doesn't
-    // see anything until their schedule is populated.
     this.visible = false;
 };
 
@@ -33,31 +27,25 @@ ScheduleManager.prototype.type = function() {
 
 ScheduleManager.prototype.addTimeBlock = function(day, start, end, groupNumber = -1) {
     var scheduleTable = document.getElementById("table");
-    var hourContainer = scheduleTable.getElementsByClassName("hour-container");
+    var hourContainers = scheduleTable.getElementsByClassName("hour-container");
     if (groupNumber != -1 && this.type == ScheduleType.FREE_TIME)
         throw "Tried to add a group time block to a free time schedule.";
     else if (!groupNumber && this.type == ScheduleType.GROUP)
         throw "Tried to add a free time block to a group schedule.";
 
     if (this.type == ScheduleType.FREE_TIME) {
-        // TODO: Quick and easy; add a free time styled block.
-        console.log("adding block");
         while (start < end) {
-          var block = (start * 7 - 1) + day;
-          hourContainer[block].style.backgroundColor = GroupColorPallet[0];
-          start++;
+            var block = (start * 7 - 1) + day;
+            hourContainers[block].style.backgroundColor = GroupColorPallet[0];
+            start++;
         }
-        return;
     }
-
-    // TODO: Add a group block using groupNumber to lookup colors in the GroupColorPallet or whatever works.
 };
 
 ScheduleManager.prototype.removeTimeBlock = function(day, start, end) {
     var scheduleTable = document.getElementById("table");
     var hourContainer = scheduleTable.getElementsByClassName("hour-container");
     while (start < end) {
-      console.log("removing block");
       var block = (start * 7 - 1) + day;
       hourContainer[block].style.backgroundColor = "#AFEFAF";
       start++;
